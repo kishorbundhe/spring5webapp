82f5be608db4b52dbd40c281c54c5760e22d5533
terrorRuntime <kishorbundhe@gmail.com>
2021-01-09 02:05:13 +0530

    brings the controller and list the authors and books

--- src/main/java/guru/springframework/spring5webapp/model/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/model/Publisher.java
@@ -1,9 +1,14 @@
 package guru.springframework.spring5webapp.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Publisher {
@@ -14,6 +19,17 @@ public class Publisher {
 	private String addressLine;
 	private String city;
 	private String zip;
+	@OneToMany
+	@JoinColumn(name="publisher_id")
+	private Set<Book> books= new HashSet<>();
+	
+	public Set<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+	}
 
 	public Publisher() {
 
